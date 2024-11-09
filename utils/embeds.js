937d5278c74638dc6060747291ae9894dcc9e236
Evicly <820491@dpsk12.net>
2024-11-09 15:48:51 -0700

    Embed Logo Fix

--- utils/embeds.js
+++ utils/embeds.js
@@ -7,7 +7,7 @@ const EMBED_COLORS = {
   WARN: '#f1c40f',
 };
 
-const BOT_LOGO_URL = 'https://cdn.glitch.global/06f76352-f966-453d-854d-61c4a0dde58a/Atherium.jpg?v=1731189527482';
+const BOT_LOGO_URL = 'https://cdn.glitch.global/06f76352-f966-453d-854d-61c4a0dde58a/Atherium.png?v=1731192508930';
 const BOT_NAME = 'Atherium';
 
 function createEmbed({ title, description, color = 'DEFAULT', footerText = '' }) {
